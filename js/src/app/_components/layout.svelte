518452cc482f00890ce2e89b26c0a0d9fbe5d4ee
lnx645 <dadanhidyt@gmail.com>
2026-05-22 00:45:28 +0700

    Refactor layout and login components for improved styling and structure

--- js/src/app/_components/layout.svelte
+++ js/src/app/_components/layout.svelte
@@ -2,8 +2,8 @@
   const { children } = $props();
 </script>
 
-<nav class="w-full h-14 bg-navbar shadow-1">
-  <div class="container mx-auto px-24 h-14 flex items-center">
+<nav class="w-full h-13 bg-navbar text-navbar-foreground shadow-elevation-2">
+  <div class="container mx-auto px-3 lg:px-24 h-13 flex items-center">
     <a href="/" class="font-bold text-lg">Sobuzz.id</a>
     <div class="ml-auto">
       <a href="/login" class="text-sm text-text-1">Login</a>
--- js/src/app/auth/login.svelte
+++ js/src/app/auth/login.svelte
@@ -1,33 +1,38 @@
 <!-- login page -->
+
 <div class="max-w-sm mx-auto mt-24">
-  <div class="shadow-card-1 border border-zinc-700 rounded-md bg-card p-4">
-    <div class="text-text-1 text-2xl mb-3">
+  <div class="bg-card shadow-elevation-1 overflow-hidden rounded-xs">
+    <div
+      class="text-text-1 text-lg font-bold mb-3 px-3 py-2 shadow-elevation-1"
+    >
       <h2>Login</h2>
     </div>
-    <form action="" class="flex flex-col gap-y-4">
+    <form action="" class="flex flex-col gap-y-4 pb-6 px-4">
       <div>
-        <label class="text-xs mb-1 inline-block" for="email">Email</label>
+        <label class="text-xs font-semibold mb-1 inline-block" for="email"
+          >Email</label
+        >
         <input
           placeholder="Ketikan email anda"
-          class="w-full rounded shadow-inset-3 align-middle border-0 input-base apparance-none text-color-2 text-sm bg-input p-2  ring-0 outline-none"
+          class="w-full rounded shadow-elevation-inset-1 align-middle border-0 input-base apparance-none text-color-2 text-sm bg-input p-2 ring-0 outline-none"
           type="text"
           id="email"
         />
       </div>
       <div>
-        <label class="text-xs mb-1 inline-block" for="katasandi"
+        <label class="text-xs font-semibold mb-1 inline-block" for="katasandi"
           >Kata Sandi</label
         >
         <input
           placeholder="Ketikan email anda"
-          class="w-full rounded shadow-inset-3 align-middle border-0 input-base apparance-none text-color-2 text-sm bg-input p-2  ring-0 outline-none"
-          type="text"
+          class="w-full rounded shadow-elevation-inset-1 align-middle border-0 input-base apparance-none text-color-2 text-sm bg-input p-2 ring-0 outline-none"
+          type="password"
           id="email"
         />
       </div>
       <div>
         <button
-          class="bg-primary p-2 text-sm w-full rounded-lg cursor-pointer hover:bg-primary-hover hover:shadow-inset-2 shadow-inset-1"
+          class="bg-primary p-2  shadow-elevation-4 text-sm w-full active:bg-button-active rounded-lg cursor-pointer hover:bg-button-hover hover:shadow-elevation-3 text-white"
           >Login</button
         >
       </div>
