257aedbda360ebe3ee3e22652a0b94960a122f26
lnx645 <dadanhidyt@gmail.com>
2026-05-22 23:54:29 +0700

    Add Input component and utility function for unique ID generation; refactor login layout

---
+++ js/src/components/input.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import { useId } from "@/core/utils";
+  import { css } from "@emotion/css";
+  import type { HTMLInputAttributes } from "svelte/elements";
+  type InputProps = HTMLInputAttributes & {
+    value: string;
+    name: string;
+    id: string;
+    label: string;
+  };
+
+  let {
+    label,
+    id = useId(),
+    value = $bindable(),
+    name,
+    ...props
+  }: InputProps = $props();
+
+  const style = css({
+    width: "100%",
+    height: "35px",
+    boxSizing: "border-box",
+    paddingInline: 6,
+    borderColor: "rgb(188, 188, 188)",
+    borderWidth: 1,
+    borderStyle: "solid",
+    outline: "none",
+    transition: "0.5s ease",
+    background: "transparent",
+    fontSize: 12,
+    borderRadius: 7,
+    color: "rgb(82, 82, 82)",
+    ":hover": {
+      borderColor: "rgb(110, 110, 110)",
+    },
+    ":active": {
+      borderColor: "rgb(110, 110, 110)",
+    },
+    ":focus": {
+      borderColor: "rgb(110, 110, 110)",
+    },
+  });
+  const labelStyle = css({
+    fontSize: 12,
+    fontWeight: 600,
+  });
+  const fieldStyle = css({
+    lineHeight:1.5
+  })
+</script>
+
+<div class={fieldStyle}>
+  <label class={labelStyle} for={id}>
+    {label}
+  </label>
+  <input bind:value type="text" {name} {id} class={style} {...props} />
+</div>
--- js/src/routes/auth/login.svelte
+++ js/src/routes/auth/login.svelte
@@ -1,11 +1,18 @@
 <script>
   import Button from "@/components/button.svelte";
+  import Input from "@/components/input.svelte";
   import { css } from "@emotion/css";
-
 </script>
-<div class={css({
-    padding:10
-})}>
-<Button id="login">Login</Button>
 
+<div
+  class={css({
+    padding: 10,
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+  })}
+>
+  <Input placeholder="Masukan nama anda" name="password" label="Email" />
+  <Input placeholder="Masukan nama anda" name="email" label="Password" />
+  <Button id="login">Login</Button>
 </div>
